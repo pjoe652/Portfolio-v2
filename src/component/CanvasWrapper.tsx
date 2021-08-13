ca134812daf118a6d0184bf548bb4c7b9ac6f060
Peter Joe <Peter.Joe@pgtrust.co.nz>
2021-08-13 18:05:09 +1200

    Revert orbit camera when not orbiting

--- src/component/CanvasWrapper.tsx
+++ src/component/CanvasWrapper.tsx
@@ -7,20 +7,20 @@ import Paperstack from '../models/Paperstack';
 import Plane from '../models/Plane';
 import Work from '../models/Work';
 
+const defaultCamera = {position: [4.5,2,4.5], near: 0.1, far: 1000, zoom: 1.3}
+
 export default function CanvasWrapper(props) {
   return (
-    <Canvas style={{"position": "fixed"}} shadows={true} camera={{position: [4.5,2,4.5], near: 0.1, far: 1000, zoom: 1.3}}>
+    <Canvas style={{"position": "fixed"}} shadows={true} camera={props.orbit ? defaultCamera : defaultCamera}>
       <hemisphereLight intensity={0.35} />
       <spotLight position={[15, 15, 15]} angle={0.3} penumbra={1} intensity={2} castShadow />
       {
         props.orbit ? 
-        <React.Fragment>
           <gridHelper args={[ 10,10, 'white', 'gray']} />
-          <Orbit />
-        </React.Fragment>
         :
-        <React.Fragment/>
+          <React.Fragment/>
       }
+      <Orbit orbitEnable={props.orbit}/>
       <Physics>
         <Plane />
         <AboutMe mainColor={props.mainColor} active={props.order === 0} enableScroll={props.enableScroll} viewMode={props.viewMode}/>
--- src/models/Orbit.js
+++ src/models/Orbit.js
@@ -1,8 +1,17 @@
 import { OrbitControls } from "@react-three/drei"
+import { useEffect, useRef } from "react"
 
 
 export default function Camera(props) {
+  const orbitRef = useRef()
+
+  useEffect(() => {
+    if (orbitRef.current && !props.orbitEnable) {
+      orbitRef.current.reset()
+    } 
+  })
+  
   return (
-    <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+    <OrbitControls ref={orbitRef} enablePan={props.orbitEnable} enableZoom={props.orbitEnable} enableRotate={props.orbitEnable} />
   )
 }
